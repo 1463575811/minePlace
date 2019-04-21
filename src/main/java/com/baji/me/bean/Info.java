5e03970ed537d7cdb88e713de3874d1e8549c633
baji <15838259752@163.com>
2019-04-21 23:02:28 +0800

    mycode

---
+++ src/main/java/com/baji/me/bean/Info.java
@@ -0,0 +1,15 @@
+package com.baji.me.bean;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * another 元气na java class!
+ *
+ * @Author: lenovo
+ * @Date: 2019/4/21
+ */
+@Data
+@Builder
+public class Info {
+}
---
+++ src/main/java/com/baji/me/bean/MySelf.java
@@ -0,0 +1,19 @@
+package com.baji.me.bean;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * another 元气na java class!
+ *
+ * @Author: lenovo
+ * @Date: 2019/4/21
+ */
+@Data
+@Builder
+public class MySelf {
+    private String name;
+    private String password;
+    private Info info ;
+
+}
